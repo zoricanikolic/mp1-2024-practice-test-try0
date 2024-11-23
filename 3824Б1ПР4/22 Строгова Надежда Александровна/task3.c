dc5e59273ef3ae7467d01024c40e98a09e879193
Alexander Sysoyev <sysoyev@vmk.unn.ru>
2024-11-23 09:55:29 +0300

    Создать структуру каталогов, раскопировать задачи
    1. Создать папки под каждую группу
    2. Создать папки под каждого студента в каждой группе
    3. Скопировать в папку каждого студента по 3 файла с постановками задач и заготовками функций

---
+++ 3824Б1ПР4/22 Строгова Надежда Александровна/task3.c
@@ -0,0 +1,5 @@
+// Задача 3. Найти максимум из вешественных чисел a и b
+double max_of_two_numbers(double a, double b)
+{
+  return 0.0;
+}
---
+++ 3824Б1ФИ1/27 Турушкин Егор Антонович/task1.c
@@ -0,0 +1,7 @@
+// Задача 1. Найти сумму целых чисел a и b.
+// Если a + b >= INT_MAX, возвращать INT_MAX
+// Если a + b <= INT_MIN, возвращать INT_MIN
+int sum_of_two_numbers(int a, int b)
+{
+  return 0;
+}
