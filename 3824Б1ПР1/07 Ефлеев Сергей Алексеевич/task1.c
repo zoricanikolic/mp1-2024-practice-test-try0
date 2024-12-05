8745bb9065b3ac03a9f14a4d1f46cb53ba4ca793
efleevS <efleev.sergey0912@gmail.com>
2024-12-05 09:30:23 +0300

    Update task1.c

--- 3824Б1ПР1/07 Ефлеев Сергей Алексеевич/task1.c
+++ 3824Б1ПР1/07 Ефлеев Сергей Алексеевич/task1.c
@@ -1,7 +1,11 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
+#include <limits.h> 
 int sum_of_two_numbers(int a, int b)
 {
+  int s = a+b;
+  if (s>=INT_MAX) return INT_MAX;
+  if else (s <= INT_MIN) return INT_MIN;
   return 0;
 }
