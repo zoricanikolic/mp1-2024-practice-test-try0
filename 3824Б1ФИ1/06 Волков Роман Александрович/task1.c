cfe424b0ac76af07610688b7fd85d1d65b8f8791
tokyoview美的 <91493271+tokyoview@users.noreply.github.com>
2024-12-05 09:33:39 +0300

    Update task1.c

--- 3824Б1ФИ1/06 Волков Роман Александрович/task1.c
+++ 3824Б1ФИ1/06 Волков Роман Александрович/task1.c
@@ -3,9 +3,9 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 {
-  if (a + b >= INT_MAX)
+  if ((a + b) >= INT_MAX)
     return INT_MAX;
-  if (a + b <= INT_MIN)
+  if ((a + b) <= INT_MIN)
     return INT_MIN;
   return a + b;
 }
