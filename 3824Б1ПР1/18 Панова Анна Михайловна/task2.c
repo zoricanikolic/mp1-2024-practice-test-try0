beeab34bc89a72a7a9ee4ab83ee81722d9d158e5
4ne4ka <opaquellet@gmail.com>
2024-12-05 09:53:00 +0300

    Update task2.c

--- 3824Б1ПР1/18 Панова Анна Михайловна/task2.c
+++ 3824Б1ПР1/18 Панова Анна Михайловна/task2.c
@@ -1,5 +1,6 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
+#include <limits.h>
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
   long long s = long long(a) * long long(b);
