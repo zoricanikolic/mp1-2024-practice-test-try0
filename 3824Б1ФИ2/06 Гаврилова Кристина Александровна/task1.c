66f7884e4410cacb45c4d71218a88f350b7f4ad1
Kristina-Ga <kristina_gavrilova_2006@list.ru>
2024-12-05 09:57:59 +0300

    Update task1.c

--- 3824Б1ФИ2/06 Гаврилова Кристина Александровна/task1.c
+++ 3824Б1ФИ2/06 Гаврилова Кристина Александровна/task1.c
@@ -4,10 +4,10 @@
 #include <limits.h>
 int sum_of_two_numbers(int a, int b)
 {
-  if (a + b > INT_MAX) {
+  if (a + b >= INT_MAX) {
     return INT_MAX;
 }
-if (a + b < INT_MIN) {
+if (a + b <= INT_MIN) {
     return INT_MIN;
 }
 else {
