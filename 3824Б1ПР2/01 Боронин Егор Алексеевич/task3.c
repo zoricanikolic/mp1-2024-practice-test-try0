471a017c7ec568e77c46becce446506368dc54bc
sannarat <sannarat45@gmail.com>
2024-12-05 09:46:27 +0300

    Update task3.c

--- 3824Б1ПР2/01 Боронин Егор Алексеевич/task3.c
+++ 3824Б1ПР2/01 Боронин Егор Алексеевич/task3.c
@@ -1,11 +1,8 @@
 // Задача 3. Найти максимум из вешественных чисел a и b
 double max_of_two_numbers(double a, double b)
 {
+  if (isnan(a)) return b;
+  if (isnan(b)) return a;
   if (a > b) return a;
-  if (a < b) return b;
-  if (a == b)
-  {
-    printf("числа равны");
-    return 0.0;
-  }
+  return b;
 }
