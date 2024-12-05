c44cf15b23270ee2b5d69e1a0acf3ac86435f507
Elestry <nikita.antonov.unn@gmail.com>
2024-12-05 10:05:35 +0300

    Complete_task1.c

--- 3824Б1ПР3/01 Антонов Никита Александрович/task1.c
+++ 3824Б1ПР3/01 Антонов Никита Александрович/task1.c
@@ -2,14 +2,11 @@
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
 #include <limits.h>
-int sum_of_two_numbers(int a, int b)
-  {
-      if ((a + b) >= INT_MAX)
-  {
-      return INT_MAX;
+int sum_of_two_numbers(int a, int b){
+  if ((a + b) >= INT_MAX){
+    return INT_MAX;
   }
-  else if ((a + b) <= INT_MIN)
-  {
-      return INT_MIN;
+  if ((a + b) >= INT_MAX){
+    return INT_MAX;
   }
 }
