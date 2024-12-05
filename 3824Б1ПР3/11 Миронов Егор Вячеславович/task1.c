1d84cde3c22a06fd487b7eba3521eca72fabedc2
Egor <122093200+itsumiq@users.noreply.github.com>
2024-12-05 10:19:33 +0300

    Update task1.c

--- 3824Б1ПР3/11 Миронов Егор Вячеславович/task1.c
+++ 3824Б1ПР3/11 Миронов Егор Вячеславович/task1.c
@@ -1,7 +1,17 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
+#include <limits.h>
+
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+    if ((b > 0 && a > INT_MAX - b) || (b < 0 && a < INT_MIN - b)) {
+        if (b > 0) {
+            return INT_MAX;
+        } else {
+            return INT_MIN;
+        }
+    }
+
+    return a + b;
 }
