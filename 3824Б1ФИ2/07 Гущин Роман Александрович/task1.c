9ce966271e96d737f3a1447c2d43ecac3197a357
romik3211 <149517130+romik3211@users.noreply.github.com>
2024-12-05 09:39:33 +0300

    Update task1.c

--- 3824Б1ФИ2/07 Гущин Роман Александрович/task1.c
+++ 3824Б1ФИ2/07 Гущин Роман Александрович/task1.c
@@ -3,8 +3,8 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 { 
-  if a+b >= INT_MAX
+  if (a+b) >= INT_MAX
     return INT_MAX;
-  if a+b <= INT_MIN
+  if (a+b) <= INT_MIN
     return INT_MIN;
 }
