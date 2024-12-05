e88827403afbf4d912f099384df1ae7269c7028f
Сергей Семеняго <135709710+DorianFryi@users.noreply.github.com>
2024-12-05 10:02:38 +0300

    Update task1.c

--- 3824Б1ПР5/22 Семеняго Сергей Павлович/task1.c
+++ 3824Б1ПР5/22 Семеняго Сергей Павлович/task1.c
@@ -1,7 +1,10 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
+#include <limits.h>
 int sum_of_two_numbers(int a, int b)
-{
-  return 0;
+{ int ans=a+b;
+ if ((ans)>=INT_MAX) ans=INT_MAX;
+ else if ((ans)<=INT_MIN) ans=INT_MIN;
+  return ans;
 }
