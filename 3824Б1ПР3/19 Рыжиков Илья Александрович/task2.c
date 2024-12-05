feb6b1d6b5764c129834cd66906a888bcd58c77c
ryzhil <119815908+ryzhil@users.noreply.github.com>
2024-12-05 09:38:20 +0300

    Update task2.c

--- 3824Б1ПР3/19 Рыжиков Илья Александрович/task2.c
+++ 3824Б1ПР3/19 Рыжиков Илья Александрович/task2.c
@@ -1,6 +1,23 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
+#include <limits.h>
+#include <stdio.h>
+
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
-  return 0;
+    if (a != 0 && b > UINT_MAX / a) {
+        return UINT_MAX;
+    }
+    return a * b;
+}
+
+int main(){
+    unsigned a, b, c;
+    scanf("%u", &a);
+    scanf("%u", &b);
+
+    c = product_of_two_numbers(a,b);
+    printf("%u", c);
+
+    return 0;
 }
