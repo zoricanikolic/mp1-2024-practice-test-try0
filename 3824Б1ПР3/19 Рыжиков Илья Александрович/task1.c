5f2cd8a75740d6fcd282c26dfc9eb353f82cea14
ryzhil <119815908+ryzhil@users.noreply.github.com>
2024-12-05 09:31:29 +0300

    Update task1.c

--- 3824Б1ПР3/19 Рыжиков Илья Александрович/task1.c
+++ 3824Б1ПР3/19 Рыжиков Илья Александрович/task1.c
@@ -1,7 +1,29 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
+#include <limits.h>
+#include <stdio.h>
+
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+    if (a + b > INT_MAX) {
+        return INT_MAX;
+    } 
+    else if (a + b < INT_MIN) {
+        return INT_MIN; 
+    } 
+    else {
+        return a + b; 
+    }
+}
+
+int main() {
+    int a, b, c;
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    c = sum_of_two_numbers(a,b);
+    printf("%d", c);
+
+    return 0;
 }
