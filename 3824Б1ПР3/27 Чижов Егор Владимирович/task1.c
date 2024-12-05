ef32c80893f365b9f60dd37619ad48d9dfc1b81d
no <78825973+Resways@users.noreply.github.com>
2024-12-05 09:50:24 +0300

    Update task1.c

--- 3824Б1ПР3/27 Чижов Егор Владимирович/task1.c
+++ 3824Б1ПР3/27 Чижов Егор Владимирович/task1.c
@@ -1,7 +1,43 @@
-// Задача 1. Найти сумму целых чисел a и b.
-// Если a + b >= INT_MAX, возвращать INT_MAX
-// Если a + b <= INT_MIN, возвращать INT_MIN
+#include <stdio.h>
+#include <limits.h>
+
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+    long long sum = (long long)a + b;
+
+    if (sum > INT_MAX) {
+        return INT_MAX;
+    } else if (sum < INT_MIN) {
+        return INT_MIN;
+    }
+
+    return (int)sum;
 }
+
+int main()
+{
+    int a = 2147483640;
+    int b = 100;
+
+int sum_of_two_numbers(int a, int b)
+{
+    long long sum = (long long)a + b;
+    if (sum > INT_MAX) {
+        return INT_MAX;
+    } else if (sum < INT_MIN) {
+        return INT_MIN;
+    }
+
+    return (int)sum;
+}
+
+    int result = sum_of_two_numbers(a, b);
+    printf("Result: %d\n", result);
+
+    return 0;
+}
+// Из-за того что функция main является входной точкой в языке C
+// Я ее добавил в самый конец своего кодаи вызываю через main первую функцию
+// И вывожу через принт, если делать без функции main получается ошибка
+// collect2: error: ld returned 1 exit status
+// также добавил 2 тестовых числа для проверки нашей программы
