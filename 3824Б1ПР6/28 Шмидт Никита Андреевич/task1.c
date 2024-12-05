e50434ff126e613d71fb5061d7e526149f49f64b
Shmudoslave <149603728+Shmudoslave@users.noreply.github.com>
2024-12-05 09:50:10 +0300

    Update task1.c

--- 3824Б1ПР6/28 Шмидт Никита Андреевич/task1.c
+++ 3824Б1ПР6/28 Шмидт Никита Андреевич/task1.c
@@ -1,6 +1,9 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
+
+#include <stdio.h>
+#include <limits.h>
 int sum_of_two_numbers(int a, int b)
 {
    if (a > 0 && b > 0 && a > INT_MAX - b) {
