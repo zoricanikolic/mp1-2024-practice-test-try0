713ea8f239579a4fd2558d1fb4adaf1c7ad2c74e
ryzhil <119815908+ryzhil@users.noreply.github.com>
2024-12-05 09:46:10 +0300

    Update task3.c

--- 3824Б1ПР3/19 Рыжиков Илья Александрович/task3.c
+++ 3824Б1ПР3/19 Рыжиков Илья Александрович/task3.c
@@ -1,5 +1,21 @@
 // Задача 3. Найти максимум из вешественных чисел a и b
+#include <stdio.h>
+
 double max_of_two_numbers(double a, double b)
 {
-  return 0.0;
+    if(a > b)
+        return a;
+    return b;
+}
+
+int main()
+{
+    double a, b, c;
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+    
+    c = max_of_two_numbers(a, b);
+    printf("%lf", c);
+
+    return 0;
 }
