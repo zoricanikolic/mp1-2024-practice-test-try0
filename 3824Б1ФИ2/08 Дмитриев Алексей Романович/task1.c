ab5da03168040e4a0b25d6d796e391ab71477897
AdLeGeR <dmitr1effal@yandex.ru>
2024-12-05 09:52:03 +0300

    удалил лишнюю строчку

--- 3824Б1ФИ2/08 Дмитриев Алексей Романович/task1.c
+++ 3824Б1ФИ2/08 Дмитриев Алексей Романович/task1.c
@@ -2,7 +2,6 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
-int sum_of_two_numbers(int a, int b);
 int sum_of_two_numbers(int a, int b)
 {
     if(a >= 0 && INT_MAX - a <= b){
