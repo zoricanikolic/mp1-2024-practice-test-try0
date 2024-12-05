118669d8b8f61fbe63ec575664f37ec96c0a7e48
AndreiStendaper <andreipirozhochek@gmail.com>
2024-12-05 09:36:34 +0300

    Update task1.c

--- 3824Б1ФИ3/06 Бочкарев Андрей Вадимович/task1.c
+++ 3824Б1ФИ3/06 Бочкарев Андрей Вадимович/task1.c
@@ -1,7 +1,11 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
+#include <limits.h>
 int sum_of_two_numbers(int a, int b)
-{
-  return 0;
+{ int ans;
+ if ((a+b)>=INT_MAX) ans=INT_MAX;
+ else if ((a+b)<=INT_MIN) ans=INT_MIN;
+ else (ans=a+b);
+  return ans;
 }
