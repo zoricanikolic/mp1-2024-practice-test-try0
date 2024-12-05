cdda28d45b78ae189e172a104002940ca3c2d073
FedorovaAlla <fedorova.alla0616@gmail.com>
2024-12-05 09:51:06 +0300

    Update task2.c

--- 3824Б1ПР1/28 Федорова Алла Максимовна/task2.c
+++ 3824Б1ПР1/28 Федорова Алла Максимовна/task2.c
@@ -1,8 +1,9 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
-{
-  if ((a * b) >= UINT_MAX)
-	  return UINT_MAX;
-  return (a * b);
+{	
+long long c = long long(a)*long long(b)
+if (c) >= UINT_MAX)
+	return UINT_MAX;
+return (a * b);
 }
