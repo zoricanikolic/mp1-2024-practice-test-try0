8aface4463f94647f4a7ac334601e6ef1561a710
AndreiStendaper <andreipirozhochek@gmail.com>
2024-12-05 09:43:17 +0300

    Update task1.c

--- 3824Б1ФИ3/06 Бочкарев Андрей Вадимович/task1.c
+++ 3824Б1ФИ3/06 Бочкарев Андрей Вадимович/task1.c
@@ -3,9 +3,8 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 #include <limits.h>
 int sum_of_two_numbers(int a, int b)
-{ int ans;
- if ((a+b)>=INT_MAX) ans=INT_MAX;
- else if ((a+b)<=INT_MIN) ans=INT_MIN;
- else (ans=a+b);
+{ int ans=a+b;
+ if ((ans)>=INT_MAX) ans=INT_MAX;
+ else if ((ans)<=INT_MIN) ans=INT_MIN;
   return ans;
 }
