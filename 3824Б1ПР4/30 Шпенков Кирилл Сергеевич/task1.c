e30bd9d08ce4014d49f06eafe8d35f9c9241122e
Magickk20 <spenkovkirill5@gmail.com>
2024-12-05 10:14:04 +0300

    Update task1.c

--- 3824Б1ПР4/30 Шпенков Кирилл Сергеевич/task1.c
+++ 3824Б1ПР4/30 Шпенков Кирилл Сергеевич/task1.c
@@ -1,7 +1,17 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
+#include <stdio.h>
+#include <limits.h> 
+
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+  if (a > 0 && b > INT_MAX - a) return INT_MAX; 
+  if (a < 0 && b < INT_MIN - a) return INT_MIN; 
+  return a + b; 
+}
+int main() {
+    int a = 76, b = 1;
+    printf("%d\n", sum_of_two_numbers(a, b));
+    return 0;
 }
