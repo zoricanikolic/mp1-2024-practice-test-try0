2d4378ed215879df8be884c9e4959fc8c06421d0
Татьяна <dumpling.lol13@gmail.com>
2024-12-05 10:10:50 +0300

    Update task3.c
    Кузьмина Татьяна Владимировна

--- 3824Б1ПР2/12 Кузьмина Татьяна Владимировна/task3.c
+++ 3824Б1ПР2/12 Кузьмина Татьяна Владимировна/task3.c
@@ -1,5 +1,14 @@
 // Задача 3. Найти максимум из вешественных чисел a и b
 double max_of_two_numbers(double a, double b)
 {
+  if(a > b){
+    return a;
+  }
+  if(a < b){
+    return b
+  }
+  if(a == b){
+    return a;
+  }
   return 0.0;
 }
