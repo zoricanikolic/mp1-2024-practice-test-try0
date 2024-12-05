7e31f21b9b2713e74102acc4eacbc45fce0cef40
emfsa <skvoznyakovmax@gmail.com>
2024-12-05 10:12:03 +0300

    Update task1.c

--- 3824Б1ПР3/23 Сквозняков Максим Андреевич/task1.c
+++ 3824Б1ПР3/23 Сквозняков Максим Андреевич/task1.c
@@ -3,5 +3,12 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+  if ((a + b) >= INT_MAX)
+  {
+  	return INT_MAX;
+  }
+  else if ((a + b) <= INT_MIN)
+  {
+  	return INT_MIN;
+  }
 }
