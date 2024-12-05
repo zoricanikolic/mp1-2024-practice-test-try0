caff7bee590389b3edfbb358623163ae68d92603
Emre-nu <emre.nural.oz@gmail.com>
2024-12-05 09:29:10 +0300

    Update task2.c

--- 3824Б1ПР3/13 Нурал Эмрэ Озгюрович/task2.c
+++ 3824Б1ПР3/13 Нурал Эмрэ Озгюрович/task2.c
@@ -1,6 +1,25 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <ctime>
+
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
-  return 0;
+	if (a * b >= UINT_MAX)
+	{
+		return UINT_MAX;
+	}
+	else
+	{
+		return a * b;
+	}
+}
+
+void main()
+{
+	int a, b;
+  scanf_s("%d %d", &a, &b);
+  printf("%d", product_of_two_numbers(a, b));
 }
