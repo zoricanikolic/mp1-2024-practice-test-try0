ccda2f067dd12d0a0f73c24b276c9c8d55c9efe8
anastaciaaaa13 <158077069+anastaciaaaa13@users.noreply.github.com>
2024-12-05 10:02:11 +0300

    Update task1.c

--- 3824Б1ПР4/17 Медведева Анастасия Павловна/task1.c
+++ 3824Б1ПР4/17 Медведева Анастасия Павловна/task1.c
@@ -1,7 +1,11 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
-int sum_of_two_numbers(int a, int b)
-{
-  return 0;
+#include <limits.h>
+
+unsigned int product_of_two_numbers(unsigned int a, unsigned int b) {
+    if (a > 0 && b > UINT_MAX / a) {
+        return UINT_MAX;
+    }
+    return a * b;
 }
