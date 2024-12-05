86341e961d611d6ab11f96edce30c400ff72d584
Kostyahard222 <84705935+Kostyahard222@users.noreply.github.com>
2024-12-05 10:06:47 +0300

    Update task1.c

--- 3824Б1ФИ2/05 Воробьев Константин Михайлович/task1.c
+++ 3824Б1ФИ2/05 Воробьев Константин Михайлович/task1.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 int sum_of_two_numbers(int a, int b)
 {
-  if(a>=INT_MAX-b) return INT_MAX;
-  if(a<=INT_MIN-b) return INT_MIN;
+  if(a+b>=INT_MAX) return INT_MAX;
+  if(a+b<=INT_MIN) return INT_MIN;
   else return a+b;
 }
