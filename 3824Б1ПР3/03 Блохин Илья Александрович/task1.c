4c4103df3a05a560c9a04025aa8d45851d0c7e86
IntFxZen <86683282+IntFxZen@users.noreply.github.com>
2024-12-05 09:49:02 +0300

    Update task1.c

--- 3824Б1ПР3/03 Блохин Илья Александрович/task1.c
+++ 3824Б1ПР3/03 Блохин Илья Александрович/task1.c
@@ -1,13 +1,15 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
+#include <limits.h>
 int sum_of_two_numbers(int a, int b)
 {
-  if (b > 0 && a > INT_MAX - b) {
-        return INT_MAX; 
+int sum = a + b;
+    if (sum > INT_MAX) {
+        return INT_MAX;
+    } else if (sum < INT_MIN) {
+        return INT_MIN;
+    } else {
+        return sum;
     }
-    if (b < 0 && a < INT_MIN - b) {
-        return INT_MIN; 
-    }
-  return a+b;
 }
