f1ebf3cba2c813842a2b0f467593646e25ae819f
efleevS <efleev.sergey0912@gmail.com>
2024-12-05 09:36:14 +0300

    Update task1.c

--- 3824Б1ПР1/07 Ефлеев Сергей Алексеевич/task1.c
+++ 3824Б1ПР1/07 Ефлеев Сергей Алексеевич/task1.c
@@ -2,9 +2,10 @@
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
 #include <limits.h> 
+#include <stdio.h>
 int sum_of_two_numbers(int a, int b)
 {
-  int s = a+b;
+  long long s = long long(a) + long long(b);
   if (s>=INT_MAX) return INT_MAX;
   if else (s <= INT_MIN) return INT_MIN;
   return 0;
