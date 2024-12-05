2db9b3e1e54693ac0a314e6bca4355b7116b2bfb
waiafur <v.o.bolshakov@gmail.com>
2024-12-05 09:47:04 +0300

    Update task2.c

--- 3824Б1ПР1/05 Большаков Владислав Олегович/task2.c
+++ 3824Б1ПР1/05 Большаков Владислав Олегович/task2.c
@@ -3,7 +3,7 @@
 #include <limits.h>
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
-  long long c = long long(a)*long long(b);
+  unsigned long long c = unsigned long long(a)*unsigned long long(b);
   if (c>=UINT_MAX) return UINT_MAX;
   return a*b;
 }
