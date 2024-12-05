728166f2dba75216ad327377fefa62f4e1276521
sannarat <sannarat45@gmail.com>
2024-12-05 09:37:33 +0300

    Update task1.c

--- 3824Б1ПР2/01 Боронин Егор Алексеевич/task1.c
+++ 3824Б1ПР2/01 Боронин Егор Алексеевич/task1.c
@@ -3,8 +3,8 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 {
-  if (a  + b >= INT_MAX) return INT_MAX;
-  if (a + b <+ INT_MIN) return INT_MIN;
+  if (a > 0 && b >= INT_MAX - a) return INT_MAX;
+  if (a < 0 && b < INT_MIN - a) return INT_MIN;
   
   return a + b;
 }
