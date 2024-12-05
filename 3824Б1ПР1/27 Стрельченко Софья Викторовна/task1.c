e4b83198ba4dae534a0042e291e8acf05d71858c
StrelchenkoSofya <sofa.strelchenko05@gmail.com>
2024-12-05 09:41:41 +0300

    Update task1.c

--- 3824Б1ПР1/27 Стрельченко Софья Викторовна/task1.c
+++ 3824Б1ПР1/27 Стрельченко Софья Викторовна/task1.c
@@ -1,7 +1,16 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
+#include<limits.h>
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+	long long sum = long long(a) + long long(b);
+	if (sum >= INT_MAX) {
+		return INT_MAX;
+	}
+	if (sum <= INT_MIN) {
+		return INT_MIN;
+	}
+	return sum;
+	return 0;
 }
