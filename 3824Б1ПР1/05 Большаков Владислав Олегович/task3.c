be61658698348f952646ae33ccef6e171f477170
waiafur <v.o.bolshakov@gmail.com>
2024-12-05 09:57:11 +0300

    Update task3.c

--- 3824Б1ПР1/05 Большаков Владислав Олегович/task3.c
+++ 3824Б1ПР1/05 Большаков Владислав Олегович/task3.c
@@ -1,4 +1,4 @@
-// Задача 3. Найти максимум из вещественных чисел a и b
+// Задача 3. Найти максимум из вешественных чисел a и b
 double max_of_two_numbers(double a, double b)
 {
   if (a>=b) return a;
