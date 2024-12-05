cee67e296ea88bc93f6b6fbdf52a72b97e7564a7
Vester07 <korolevpetr01@gmail.com>
2024-12-05 10:01:06 +0300

    Update task2.c
    ступапупа

--- 3824Б1ПР5/12 Королев Пётр Александрович/task2.c
+++ 3824Б1ПР5/12 Королев Пётр Александрович/task2.c
@@ -1,6 +1,24 @@
-// Задача 2. Найти произведение натуральных чисел a и b
-// Если a * b >= UINT_MAX, возвращать UINT_MAX
-unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
-{
-  return 0;
+#include <stdio.h>
+#include <limits.h>
+
+unsigned int product_of_two_numbers(unsigned int a, unsigned int b) {
+    if (a > 0 && b > 0 && a > UINT_MAX / b) {
+        return UINT_MAX;  
+    }
+    return a * b; 
+}
+
+int main() {
+    unsigned int a, b;
+
+    printf("Введите первое натуральное число (a): ");
+    scanf("%u", &a);
+    printf("Введите второе натуральное число (b): ");
+    scanf("%u", &b);
+
+    unsigned int result = product_of_two_numbers(a, b);
+
+    printf("Произведение: %u\n", result);
+
+    return 0;
 }
