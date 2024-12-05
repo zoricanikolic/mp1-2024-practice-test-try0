4be6efd7d01aa13ad8ac74fe3c22dcc4a14721b1
no <78825973+Resways@users.noreply.github.com>
2024-12-05 09:54:33 +0300

    Update task2.c

--- 3824Б1ПР3/27 Чижов Егор Владимирович/task2.c
+++ 3824Б1ПР3/27 Чижов Егор Владимирович/task2.c
@@ -1,6 +1,29 @@
-// Задача 2. Найти произведение натуральных чисел a и b
-// Если a * b >= UINT_MAX, возвращать UINT_MAX
+#include <stdio.h>
+#include <limits.h>
+
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
-  return 0;
+    unsigned long long product = (unsigned long long)a * b;
+
+    if (product > UINT_MAX) {
+        return UINT_MAX;
+    }
+
+    return (unsigned int)product;
 }
+
+int main()
+{
+    unsigned int a = 2000000000;
+    unsigned int b = 3;        
+
+    unsigned int result = product_of_two_numbers(a, b);
+    printf("Product: %u\n", result);
+
+    return 0;
+}
+// Из-за того что функция main является входной точкой в языке C
+// Я ее добавил в самый конец своего кода вызываю через main первую функцию
+// И вывожу через принт, если делать без функции main на выводе будет ошибка
+// collect2: error: ld returned 1 exit status
+// также добавил 2 тестовых числа для проверки нашей программы
