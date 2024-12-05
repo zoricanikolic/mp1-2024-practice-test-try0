f6448b6ce0206c36504347cf82117b3790785641
seiftnesse <51361929+seiftnesse@users.noreply.github.com>
2024-12-04 23:08:19 -0800

    Update task3.c

--- 3824Б1ПР5/02 Баландин Владислав Вячеславович/task3.c
+++ 3824Б1ПР5/02 Баландин Владислав Вячеславович/task3.c
@@ -1,8 +1,5 @@
 // Задача 3. Найти максимум из вешественных чисел a и b
 double max_of_two_numbers(double a, double b)
-    if (isnan(a) || isnan(b)) {
-        return NAN;
-    }
-    
-    return (a > b) ? a : b;
+{
+    return fmax(a, b);
 }
