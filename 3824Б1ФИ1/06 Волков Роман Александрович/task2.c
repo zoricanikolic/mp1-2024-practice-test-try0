50de9061b9c53b6bc0b18b4749926dd0855808e3
tokyoview美的 <91493271+tokyoview@users.noreply.github.com>
2024-12-05 09:55:57 +0300

    Update task2.c

--- 3824Б1ФИ1/06 Волков Роман Александрович/task2.c
+++ 3824Б1ФИ1/06 Волков Роман Александрович/task2.c
@@ -1,8 +1,10 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
+#include <limits.h>
+
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
-  long long unsigned int res = (long long unsigned int)(a * b);
+  long long unsigned int res = (long long unsigned int)(a) * (long long unsigned int)(b);
   if (res >= UINT_MAX)
     return UINT_MAX;
   return a * b;
