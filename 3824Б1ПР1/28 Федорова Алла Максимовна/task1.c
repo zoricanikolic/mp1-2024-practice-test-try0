eed7813b022391e7ba9788faab8b249d69995fbe
FedorovaAlla <fedorova.alla0616@gmail.com>
2024-12-05 09:38:33 +0300

    Update task1.c

--- 3824Б1ПР1/28 Федорова Алла Максимовна/task1.c
+++ 3824Б1ПР1/28 Федорова Алла Максимовна/task1.c
@@ -3,5 +3,9 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+	if ((a + b) >= INT_MAX)
+		return INT_MAX;
+	if ((a + b) <= INT_MIN)
+		return INT_MIN;
+	return (a+b);
 }
