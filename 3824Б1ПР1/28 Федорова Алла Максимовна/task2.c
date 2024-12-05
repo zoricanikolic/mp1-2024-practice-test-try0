6a1f31841ae3e0837a697dbb4002c90a163ee65a
FedorovaAlla <fedorova.alla0616@gmail.com>
2024-12-05 09:51:36 +0300

    Update task2.c

--- 3824Б1ПР1/28 Федорова Алла Максимовна/task2.c
+++ 3824Б1ПР1/28 Федорова Алла Максимовна/task2.c
@@ -2,7 +2,7 @@
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {	
-long long c = long long(a)*long long(b)
+long long c = long long(a)*long long(b);
 if (c >= UINT_MAX)
 	return UINT_MAX;
 return (a * b);
