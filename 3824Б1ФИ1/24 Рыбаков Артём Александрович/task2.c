3d330759a7e336df259f671c02ce4585b8a2adaf
Clearnery <artemrybakov@outlook.com>
2024-12-05 10:03:22 +0300

    Update task2.c
    Рыбаков Артем Александрович

--- 3824Б1ФИ1/24 Рыбаков Артём Александрович/task2.c
+++ 3824Б1ФИ1/24 Рыбаков Артём Александрович/task2.c
@@ -1,6 +1,15 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
+#include<limits.h>
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
-  return 0;
+  insigned long long int c;
+  if (c > UINT_MAX)
+  {
+    return UINT_MAX;
+  }
+  else
+  {
+    return c;
+  }
 }
