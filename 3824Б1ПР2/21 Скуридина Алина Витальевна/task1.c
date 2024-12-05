ce1876db8b150cc1b83dd34e91ea8463c14680e7
Алина Скуридина <94843385+Allynall@users.noreply.github.com>
2024-12-05 09:47:13 +0300

    Update task1.c

--- 3824Б1ПР2/21 Скуридина Алина Витальевна/task1.c
+++ 3824Б1ПР2/21 Скуридина Алина Витальевна/task1.c
@@ -5,7 +5,7 @@
 int sum_of_two_numbers(int a, int b)
 {
   	if ((a + b) >= INT_MAX) { return INT_MAX; }
-	if ((a + b) <= INT_MIN) { return INT_MIN; }
-	return a + b;
+	else if ((a + b) <= INT_MIN) { return INT_MIN; }
+	else { return a + b; }
 
 }
