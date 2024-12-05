da639922e057a3acf564a32fa9419b44415de485
Dmitry Katyshev <181271866+Amour21@users.noreply.github.com>
2024-12-05 09:50:13 +0300

    Update task2.c

--- 3824Б1ФИ1/15 Катышев Дмитрий Владимирович/task2.c
+++ 3824Б1ФИ1/15 Катышев Дмитрий Владимирович/task2.c
@@ -1,5 +1,8 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
+#include <stdio.h>
+#include <limits.h>
+
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
     unsigned long long mult = a * b;
