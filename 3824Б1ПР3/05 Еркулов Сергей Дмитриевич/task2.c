9655bd6343778ab8bda5780009262c3b9cf4ba79
tulenb52 <51734791+tulenb52@users.noreply.github.com>
2024-12-05 10:01:27 +0300

    Update task2.c

--- 3824Б1ПР3/05 Еркулов Сергей Дмитриевич/task2.c
+++ 3824Б1ПР3/05 Еркулов Сергей Дмитриевич/task2.c
@@ -1,6 +1,11 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
-unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
-{
-  return 0;
+unsigned int product_of_two_numbers(unsigned int a, unsigned int b) {
+    unsigned long long proizved = (unsigned long long)a * (unsigned long long)b;
+
+    if (proizved >= UINT_MAX) {
+        return UINT_MAX;
+    } else {
+        return proizved;
+    }
 }
