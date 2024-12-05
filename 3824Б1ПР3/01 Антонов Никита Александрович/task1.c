6d1834d7f3375f178a3e472f9b290f4c3c7d34ae
Elestry <nikita.antonov.unn@gmail.com>
2024-12-05 10:08:28 +0300

    Update task1.c

--- 3824Б1ПР3/01 Антонов Никита Александрович/task1.c
+++ 3824Б1ПР3/01 Антонов Никита Александрович/task1.c
@@ -6,7 +6,7 @@ int sum_of_two_numbers(int a, int b){
   if ((a + b) >= INT_MAX){
     return INT_MAX;
   }
-  if ((a + b) >= INT_MAX){
-    return INT_MAX;
+  else if ((a + b) <= INT_MIN){
+    return INT_MIN;
   }
 }
