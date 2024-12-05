2de5aac3fe46cab64f8749890750d3c2152460fc
2616181015 <artem30112005art@gmail.com>
2024-12-05 09:53:04 +0300

    Update task1.c

--- 3824Б1ПР4/28 Шорин Артём Алексеевич/task1.c
+++ 3824Б1ПР4/28 Шорин Артём Алексеевич/task1.c
@@ -2,6 +2,8 @@
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
 
+#include <limits.h>
+
 int sum_of_two_numbers(int a, int b)
 {
   if (a > 0 && b > INT_MAX - a) return INT_MAX;
