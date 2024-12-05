2c84a6f2d9997ee8c02a1830dfbadfdfd4f40616
rat-noob <romakapranov16@gmail.com>
2024-12-05 10:05:35 +0300

    Update task2.c

--- 3824Б1ПР1/09 Капранов Роман Валерьевич/task2.c
+++ 3824Б1ПР1/09 Капранов Роман Валерьевич/task2.c
@@ -1,7 +1,5 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
-#include <stdlib.h>
-#include <stdio.h>
 #include<limits.h>
 
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
