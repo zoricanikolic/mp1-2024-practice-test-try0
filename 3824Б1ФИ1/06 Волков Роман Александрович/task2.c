cc4e11e88880c45c973fb68c6169b5f423618fd7
tokyoview美的 <91493271+tokyoview@users.noreply.github.com>
2024-12-05 09:43:09 +0300

    Update task2.c

--- 3824Б1ФИ1/06 Волков Роман Александрович/task2.c
+++ 3824Б1ФИ1/06 Волков Роман Александрович/task2.c
@@ -2,7 +2,8 @@
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
-  if ((a * b) >= UINT_MAX)
+  long long unsigned int res = (long long unsigned int)(a * b);
+  if (res >= UINT_MAX)
     return UINT_MAX;
   return a * b;
 }
