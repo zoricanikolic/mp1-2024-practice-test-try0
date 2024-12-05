f2971a4c9603c87bc0d2b6b2c33562c1c41e5a19
bogdana-sazonova <bogdanasazonova45@gmail.com>
2024-12-05 09:26:27 +0300

    Update task1.c

--- 3824Б1ПР1/24 Сазонова Богдана Сергеевна/task1.c
+++ 3824Б1ПР1/24 Сазонова Богдана Сергеевна/task1.c
@@ -1,7 +1,17 @@
+#include <stdio.h>
+#include <limits.h>
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
-int sum_of_two_numbers(int a, int b)
-{
-  return 0;
+int sum_of_two_numbers(int a, int b) {
+
+	if ((a + b) >= INT_MAX) {
+		return INT_MAX;
+	}
+	if ((a + b) <= INT_MIN) {
+		return INT_MIN;
+	}
+	else { return a + b; }
+
+	return 0;
 }
