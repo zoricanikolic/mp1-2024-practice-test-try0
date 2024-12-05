6a82e4c04717f5d4324e9efc6c69e03f82f34551
Valerian <139715129+Molingen@users.noreply.github.com>
2024-12-05 09:26:45 +0300

    Update task1.c

--- 3824Б1ФИ1/03 Бессолицын Валерий Вячеславович/task1.c
+++ 3824Б1ФИ1/03 Бессолицын Валерий Вячеславович/task1.c
@@ -7,7 +7,7 @@ int sum_of_two_numbers(int a, int b) {
     if (a > 0 && b > 0 && a >= INT_MAX - b) {
         return INT_MAX;
     }
-    if (a < 0 && b < 0 && a <= INT_MIN - b) {
+    if (a < 0 && b < 0 && a <= INT_MIN + b) {
         return INT_MIN;
     }
   }
