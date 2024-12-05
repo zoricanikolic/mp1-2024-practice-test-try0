cde9ea4b3108aa478914834f0b89f0be2091db38
VadimKlimenko316 <klimvadim05@gmail.com>
2024-12-05 10:04:27 +0300

    Update task3.c

--- 3824Б1ПР4/12 Клименко Вадим Николаевич/task3.c
+++ 3824Б1ПР4/12 Клименко Вадим Николаевич/task3.c
@@ -1,6 +1,9 @@
 // Задача 3. Найти максимум из вешественных чисел a и b
-#include <math.h>
 
 double max_of_two_numbers(double a, double b) {
-  return fmax(a, b);
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
 }
