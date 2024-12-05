a1263fbf08409613a71c6009bc14095971bbaee9
Екатерина Серова <katerinaserova505@gmail.com>
2024-12-05 10:07:06 +0300

    Update task3.c

--- 3824Б1ПР1/25 Серова Екатерина Борисовна/task3.c
+++ 3824Б1ПР1/25 Серова Екатерина Борисовна/task3.c
@@ -1,5 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+
 // Задача 3. Найти максимум из вешественных чисел a и b
 double max_of_two_numbers(double a, double b)
 {
-  return 0.0;
+	if (fabs(a - b) < 1e-10) return a;
+	if (a - b > 1e-10) {
+		return a;
+	}
+	else if (b - a > 1e-10) {
+		return b;
+	}
+
 }
